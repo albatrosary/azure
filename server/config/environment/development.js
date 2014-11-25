c5e1468316edec0ee7b3b631f65777e92bb99c43
Fumio SAGAWA <fumio.sagawa@gmail.com>
2014-11-25 13:28:50 +0900

    first commit

---
+++ server/config/environment/development.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// Development specific configuration
+// ==================================
+module.exports = {
+  // MongoDB connection options
+  mongo: {
+    uri: 'mongodb://localhost/azure-dev'
+  },
+
+  seedDB: true
+};
