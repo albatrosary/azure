df651e844d9816425535b02a91af778c05084d1b
Fumio SAGAWA <fumio.sagawa@gmail.com>
2014-11-26 15:57:52 +0900

    Microsoft Azureのモバイルサービスを利用

--- client/app/sample/sample.controller.js
+++ client/app/sample/sample.controller.js
@@ -2,5 +2,42 @@
 
 angular.module('azureApp')
   .controller('SampleCtrl', function ($scope) {
-    $scope.message = 'Hello';
+  	
+    var client = new WindowsAzure.MobileServiceClient(
+      "https://albass.azure-mobile.net/",
+      "EZNfvLQZgxJtikwBcolHMLdccLPPYj45"
+	);
+
+	client.getTable("Item").read()
+		.done(
+		    function (results) {
+		         console.log("results: " + results) ;           
+		        try {
+		            console.log("exists");
+		            $scope.items = results;        
+		        }
+		        catch(err) {
+		              console.log("doesnt exist yet");  
+		        }            
+		    } 
+		);
+
+	// var item = { text: "すばらしいアイテム" };
+	// client.getTable("Item").insert(item);
+
+	// var item = { text: "それから" };
+	// client.getTable("Item").insert(item);
+
+	// var item = { text: "ほんでもって" };
+	// client.getTable("Item").insert(item);
+
+	// var item = { text: "なんでやねん" };
+	// client.getTable("Item").insert(item);
+
+	// var item = { text: "ほなそしましょかー" };
+	// client.getTable("Item").insert(item);
+
+	// var item = { text: "ほんまかいな" };
+	// client.getTable("Item").insert(item);
+
   });
